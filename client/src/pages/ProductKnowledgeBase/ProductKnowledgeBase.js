6566410cac73fec26cd9ccb8244098a65f7ed2e2
steven <steven.joo@live.ca>
2024-04-05 04:53:31 -0700

    start product page

--- client/src/pages/ProductKnowledgeBase/ProductKnowledgeBase.js
+++ client/src/pages/ProductKnowledgeBase/ProductKnowledgeBase.js
@@ -3,6 +3,33 @@ import './ProductKnowledgeBase.scss'
 
 export default function ProductKnowledgeBase() {
   return (
-    <div>ProductKnowledgeBase</div>
+    <main>
+      <section className='product'>
+        <section className='product__container'>
+          <h1 className='product__heading'>
+            Product Knowledge Base
+          </h1>
+        </section>
+        <section className='product__container'>
+          <h3 className='product__text'>
+           Product Pamphlets
+          </h3>
+          <div className='product__card'></div>
+        </section>
+        <section className='product__container'>
+          <h3 className='product__text'>
+            How-To Videos
+          </h3>
+          <div className='product__card'></div>
+        </section>
+        <section className='product__container'>
+          <h3 className='product__text'>
+            Articles
+          </h3>
+          <div className='product__card'></div>
+        </section>  
+      </section>
+      
+    </main>
   )
 }

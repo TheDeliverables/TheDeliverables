415b5cef7c33a1fe06c9cb25480133812c4e3e6f
Jovan <Qeleshi.Jovan@gmail.com>
2024-04-05 13:32:25 -0400

    Add scafolding for product page

--- client/src/components/ArticleCard/ArticleCard.js
+++ client/src/components/ArticleCard/ArticleCard.js
@@ -1,10 +1,17 @@
 import "./ArticleCard.scss"
 
-function ArticleCard(){
-    return(
+import harnessImg from '../../assets/images/helper_Images/pamphlet_HarnessPDF.png'
+
+function ArticleCard() {
+    return (
         <>
             <div className="card" >
-                <img src="" />
+                <img src={harnessImg} />
+                <div className="card__textContent" >
+                    <p>"Pamphlet Title"</p>
+                    <p>Pamphlet Sub Title</p>
+                </div>
+                <button>View PROP_TYPE</button>
             </div>
         </>
     )
--- client/src/pages/ProductKnowledgeBase/ProductKnowledgeBase.js
+++ client/src/pages/ProductKnowledgeBase/ProductKnowledgeBase.js
@@ -9,85 +9,86 @@ export default function ProductKnowledgeBase() {
 
   useEffect(() => {
     const fetchAll = async () => {
-        try {
-            const response = await axios.get(
-                'http://localhost:8080/helper/1/'
-            );
-            console.log(response.data)
+      try {
+        const response = await axios.get(
+          'http://localhost:8080/helper/1/'
+        );
+        console.log(response.data)
 
-        } catch (error) {
-            console.error('Failed to fetch information', error);
-        }
+      } catch (error) {
+        console.error('Failed to fetch information', error);
+      }
     };
 
     fetchAll();
 
-}, []);
+  }, []);
 
 
-useEffect(() => {
-  const fecthVideo = async () => {
+  useEffect(() => {
+    const fecthVideo = async () => {
       try {
-          const response = await axios.get(
-              'http://localhost:8080/helper/1/videos/1'
-          );
-          console.log(response.data)
+        const response = await axios.get(
+          'http://localhost:8080/helper/1/videos/1'
+        );
+        console.log(response.data)
 
       } catch (error) {
-          console.error('Failed to fetch video', error);
+        console.error('Failed to fetch video', error);
       }
-  };
+    };
 
-  fecthVideo();
+    fecthVideo();
 
-}, []);
+  }, []);
 
-useEffect(() => {
-  const fetchArticle = async () => {
+  useEffect(() => {
+    const fetchArticle = async () => {
       try {
-          const response = await axios.get(
-              'http://localhost:8080/helper/1/articles/1'
-          );
-          console.log(response.data)
+        const response = await axios.get(
+          'http://localhost:8080/helper/1/articles/1'
+        );
+        console.log(response.data)
 
       } catch (error) {
-          console.error('Failed to fetch video', error);
+        console.error('Failed to fetch video', error);
       }
-  };
+    };
 
-  fetchArticle();
+    fetchArticle();
 
-}, []);
+  }, []);
 
   return (
     <main>
       <section className='product'>
+          <h1 className='product__heading'> Product Knowledge Base </h1>
+
         <section className='product__container'>
-          <h1 className='product__heading'>
-            Product Knowledge Base
-          </h1>
-          <ArticleCard />
+          <h1 className='product__container-heading'>Product Pamphlets</h1>
+          <section className='product__container-cards' >
+            <ArticleCard />
+            <ArticleCard />
+          </section>
         </section>
+
         <section className='product__container'>
-          <h3 className='product__text'>
-           Product Pamphlets
-          </h3>
-          <div className='product__card'></div>
+          <h1 className='product__container-heading'>How-To Videos</h1>
+          <section className='product__container-cards' >
+            <ArticleCard />
+            <ArticleCard />
+          </section>
         </section>
+
         <section className='product__container'>
-          <h3 className='product__text'>
-            How-To Videos
-          </h3>
-          <div className='product__card'></div>
+          <h1 className='product__container-heading'>Article</h1>
+          <section className='product__container-cards' >
+            <ArticleCard />
+            <ArticleCard />
+          </section>
         </section>
-        <section className='product__container'>
-          <h3 className='product__text'>
-            Articles
-          </h3>
-          <div className='product__card'></div>
-        </section>  
+
       </section>
-      
     </main>
   )
 }

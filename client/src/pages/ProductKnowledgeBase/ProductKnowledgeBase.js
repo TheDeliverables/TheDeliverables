ceb713faaa6c5c197a13ebb2b74d41eb8005dc86
nvere <nicolette.vere@gmail.com>
2024-04-05 10:00:24 -0600

    add axios

--- client/src/pages/ProductKnowledgeBase/ProductKnowledgeBase.js
+++ client/src/pages/ProductKnowledgeBase/ProductKnowledgeBase.js
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './ProductKnowledgeBase.scss'
+import axios from "axios"
+
 
 export default function ProductKnowledgeBase() {
+
+
+  useEffect(() => {
+    const fetchAll = async () => {
+        try {
+            const response = await axios.get(
+                'http://localhost:8080/helper/1/'
+            );
+            console.log(response)
+
+        } catch (error) {
+            console.error('Failed to fetch information', error);
+        }
+    };
+
+    fetchAll();
+
+}, []);
+
+
+useEffect(() => {
+  const fechVideo = async () => {
+      try {
+          const response = await axios.get(
+              'http://localhost:8080/helper/1/videos/1'
+          );
+          console.log(response)
+
+      } catch (error) {
+          console.error('Failed to fetch video', error);
+      }
+  };
+
+  fechVideo();
+
+}, []);
+
+
   return (
     <main>
       <section className='product'>

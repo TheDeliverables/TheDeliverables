630f88f0a58c6204b4f5650bf185989af2b641bd
nvere <nicolette.vere@gmail.com>
2024-04-05 12:02:35 -0600

    update info

--- client/src/components/ArticleCard/ArticleCard.js
+++ client/src/components/ArticleCard/ArticleCard.js
@@ -1,11 +1,53 @@
 import "./ArticleCard.scss"
+import axios from "axios"
+import React, { useState, useEffect } from 'react'
 
 import harnessImg from '../../assets/images/helper_Images/pamphlet_HarnessPDF.png'
 
 function ArticleCard(props) {
-    console.log(props.image)
-    function onClickHandler(){
+    const [products, setProducts] = useState([])
+    const [videos, setVideos] = useState([])
+    const [articles, setArticles] = useState([])
+    const [details, setProductsDetails] = useState([])
 
+    useEffect(() => {
+        const fecthVideo = async () => {
+            try {
+                const response = await axios.get(
+                    'http://localhost:8080/helper/1/videos/' + props.id
+                );
+                setVideos(response.data)
+      
+            } catch (error) {
+                console.error('Failed to fetch video', error);
+            }
+        };
+      
+        fecthVideo();
+      
+      }, []);
+      
+      useEffect(() => {
+        const fetchArticle = async () => {
+            try {
+                const response = await axios.get(
+                    'http://localhost:8080/helper/1/articles/' + props.id
+                );
+                setArticles(response.data)
+      
+            } catch (error) {
+                console.error('Failed to fetch artlces', error);
+            }
+        };
+      
+        fetchArticle();
+      
+      }, []);
+
+
+
+    function onClickHandler(){
+        
     }
     return (
         <>
@@ -15,7 +57,7 @@ function ArticleCard(props) {
                     <p>{props.itemName}</p>
                     <p>{props.description}</p>
                 </div>
-                <button>View {props.type} onClick={onClickHandler}</button>
+                <button onClick={onClickHandler} id={props.type}>View {props.type}</button>
             </div>
         </>
     )

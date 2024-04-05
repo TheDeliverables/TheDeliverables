e9df10671e33d0f1ea6f91edb942d4da03c716d1
Jovan <Qeleshi.Jovan@gmail.com>
2024-04-04 20:20:17 -0400

    Add database foundations

---
+++ server/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT;
+
+// const warehouseRoutes = require("./routes/warehouse-routes");
+// const inventoryRoutes = require("./routes/inventory-routes");
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+// app.use("/api/warehouses", warehouseRoutes);
+
+// app.use("/api/inventories", inventoryRoutes);
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));

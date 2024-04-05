0f103060f48e7bd1812c9bc01ba4954ba6687929
steven <steven.joo@live.ca>
2024-04-04 23:45:29 -0700

    change background color

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,13 +4,13 @@ import "./Footer.scss"
 
 export default function Footer() {
   return (
-    <footer>
-        <hr></hr>
-        <section>
-            <p2>Refund Policy</p2>
-            <p2>Shipping Policy</p2>
-            <p2>Privacy Policy</p2>
-            <p2>Terms of Service</p2>
+    <footer className='footer'>
+        <hr className='footer__divider' />
+        <section className='footer__text-container'>
+            <p2 className="footer__text">Refund Policy</p2>
+            <p2 className="footer__text">Shipping Policy</p2>
+            <p2 className="footer__text">Privacy Policy</p2>
+            <p2 className="footer__text">Terms of Service</p2>
         </section>
     </footer>
   )
